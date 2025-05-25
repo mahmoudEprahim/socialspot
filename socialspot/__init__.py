daf5dd9bd0040ae53adc9574a0c936cd6d2bdb24
mohamedebra <mohamed.3wad20@gmail.com>
2025-05-25 20:55:10 +0300

    ds

--- socialspot/__init__.py
+++ socialspot/__init__.py
@@ -1,12 +1,12 @@
-"""socialspot""" # اسم المشروع الجديد
+"""socialspot""" 
 
 __title__ = ''
-__package__ = 'socialspot' # ممتاز، تم التعديل هنا
+__package__ = 'socialspot'
 __author__ = ''
 __author_email__ = ''
 
 from .__version__ import __version__
-from .checking import socialspot as search # تم تغيير maigret إلى socialspot
-from .socialspot import main as cli # تم تغيير maigret إلى socialspot (اسم الملف والدالة)
-from .sites import socialspotEngine, socialspotSite, socialspotDatabase # تم تغيير Maigret إلى socialspot في أسماء الفئات
+from .checking import socialspot as search
+from .socialspot import main as cli 
+from .sites import socialspotEngine, socialspotSite, socialspotDatabase
 from .notify import QueryNotifyPrint as Notifier
